d1c10934eccfd3b348e5e56983b5ddb84ecf18fc
stegmuel <thomas.steg87@gmail.com>
2020-03-07 12:45:05 +0100

    Un-normalized the data, solved the size problem

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -3,6 +3,7 @@ import numpy as np
 from preprocessing.preprocessing import *
 import matplotlib.pyplot as plt
 import librosa.display
+import math
 import torch
 from torch import nn
 
@@ -20,8 +21,9 @@ class DatasetBeethoven(data.Dataset):
         self.fs = fs
         self.ratio = ratio
         self.overlap = overlap
-        self.window_length = 8000
+        self.window_length = 8192
         self.window_number = self.compute_window_number()
+        self.hanning_length = 101
 
     def compute_window_number(self):
         """
@@ -30,7 +32,7 @@ class DatasetBeethoven(data.Dataset):
         """
         num = self.data.shape[1] - self.window_length
         den = self.window_length * (1 - self.overlap)
-        return num / den + 1
+        return math.ceil(num / den) + 1
 
     def __len__(self):
         """
@@ -39,9 +41,22 @@ class DatasetBeethoven(data.Dataset):
         """
         return self.data.shape[0] * self.window_number
 
+    def __pad_signal(self, x):
+        """
+        Adds zero-padding at the end of the last window
+        :param x: Signal with length smaller than the window length
+        :return: Padded signal
+        """
+        # Apply hanning window to avoid alliasing
+        half_hanning = np.hanning(self.hanning_length)[self.hanning_length // 2:]
+        x[- self.hanning_length // 2:] = x[- self.hanning_length // 2:] * half_hanning
+
+        padding = self.window_length - x.shape[0]
+        return np.concatenate([x, np.zeros(padding)])
+
     def __getitem__(self, index):
         """
-        Loads a single pair (x_h, x_l) of length 8000 sampled at 16 kHz for x_l
+        Loads a single pair (x_h, x_l) of length 8192 sampled at 16 kHz for x_l
         :param index: index of the sample to load
         :return: corresponding image
         """
@@ -50,6 +65,11 @@ class DatasetBeethoven(data.Dataset):
         signal = self.data[signal_index]
         window_start = int(window_index * (1 - self.overlap) * self.window_length)
         x_h = signal[window_start: window_start + self.window_length]
+
+        # Add padding for last window
+        if x_h.shape != self.window_length:
+            x_h = self.__pad_signal(x_h)
+
         x_l = upsample(downsample(x_h, self.ratio), self.ratio)
         X_h_db = compute_spectrogram(x_h)
         X_l_db = compute_spectrogram(x_l)
@@ -57,7 +77,7 @@ class DatasetBeethoven(data.Dataset):
 
 
 def main():
-    datapath = '/media/thomas/Samsung_T5/VITA/data/music/music_train.npy'
+    datapath = '/media/thomas/Samsung_T5/VITA/data/music/music_train_.npy'
     fs = 16000
     ratio = 8
     overlap = 0.5
@@ -65,7 +85,7 @@ def main():
     print(dataset.__len__())
     print(dataset.window_number)
 
-    x_h, x_l, X_h_db, X_l_db = dataset.__getitem__(18)
+    x_h, x_l, X_h_db, X_l_db = dataset.__getitem__(5)
 
     # Plotting
     fig, axes = plt.subplots(1, 2, figsize=(14, 7))
@@ -74,20 +94,6 @@ def main():
     librosa.display.specshow(X_l_db, sr=fs, x_axis='time', y_axis='hz', ax=axes[1])
     axes[1].set_title('Low quality, spectrogram', fontsize=16)
     plt.show()
-
-
-    # pixel_shuffle = torch.nn.PixelShuffle(2)
-    # gt = torch.randn(10, 4, 8, 8)
-    # gt.requires_grad_(True)
-    #
-    # print(gt.requires_grad)
-    # x = torch.randn(10, 16, 4, 4)
-    # output = pixel_shuffle.forward(x)
-    # loss_funtction = nn.MSELoss()
-    # loss = loss_funtction(gt, output)
-    # loss.backward()
-    # test = gt.con
-    # print(output.shape)
 
 
 if __name__ == '__main__':
