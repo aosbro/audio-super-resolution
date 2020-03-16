fe04aff359c57916f27b8142e0df688f342baa14
stegmuel <thomas.steg87@gmail.com>
2020-03-16 16:23:45 +0100

    Minor changes for colab

--- blocks/base_block.py
+++ blocks/base_block.py
@@ -3,7 +3,7 @@ import torch
 
 
 class BaseBlock(nn.Module):
-    def __init__(self, in_channels, kernel_sizes, channel_sizes, bottleneck_channels):
+    def __init__(self, in_channels, kernel_sizes, channel_sizes, bottleneck_channels, use_bottleneck=True):
         """
         Initializes the class BaseBlock which is the parent class to all blocks
         :param in_channels: Number of input channel
@@ -15,22 +15,35 @@ class BaseBlock(nn.Module):
         super(BaseBlock, self).__init__()
         paddings = [(kernel_size - 1) // 2 for kernel_size in kernel_sizes]
         n_scales = len(kernel_sizes)
-        self.conv_layers_1 = nn.ModuleList([nn.Conv1d(in_channels=in_channels,
-                                                      out_channels=bottleneck_channels,
-                                                      kernel_size=1)
-                                            for i in range(n_scales)])
-        self.conv_layers_2 = nn.ModuleList([nn.Conv1d(in_channels=bottleneck_channels,
-                                                      out_channels=channel_size,
-                                                      kernel_size=kernel_size,
-                                                      padding=padding)
-                                            for kernel_size, channel_size, padding in zip(kernel_sizes, channel_sizes,
-                                                                                          paddings)])
+        self.use_bottleneck = use_bottleneck
+
+        # Define convolutional layers
+        if use_bottleneck:
+            self.conv_layers_1 = nn.ModuleList([nn.Conv1d(in_channels=in_channels,
+                                                          out_channels=bottleneck_channels,
+                                                          kernel_size=1)
+                                                for i in range(n_scales)])
+            self.conv_layers_2 = nn.ModuleList([nn.Conv1d(in_channels=bottleneck_channels,
+                                                          out_channels=channel_size,
+                                                          kernel_size=kernel_size,
+                                                          padding=padding)
+                                                for kernel_size, channel_size, padding in zip(kernel_sizes, channel_sizes,
+                                                                                              paddings)])
+        else:
+            self.conv_layers = nn.ModuleList([nn.Conv1d(in_channels=in_channels,
+                                                        out_channels=channel_size,
+                                                        kernel_size=kernel_size,
+                                                        padding=padding)
+                                              for kernel_size, channel_size, padding in zip(kernel_sizes, channel_sizes,
+                                                                                            paddings)])
 
     def forward_base(self, x):
         """
         :param x: Input feature map
         :return: Stacked 
         """
-        x = [conv_layer(x) for conv_layer in self.conv_layers_1]
-        x = torch.cat(list(map(lambda temp_x, conv_layer: conv_layer(temp_x), x, self.conv_layers_2)), dim=1)
-        return x
+        if self.use_bottleneck:
+            x = [conv_layer(x) for conv_layer in self.conv_layers_1]
+            x = torch.cat(list(map(lambda temp_x, conv_layer: conv_layer(temp_x), x, self.conv_layers_2)), dim=1)
+            return x
+        return torch.cat([conv_layer(x) for conv_layer in self.conv_layers])
