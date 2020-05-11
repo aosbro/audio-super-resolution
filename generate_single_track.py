abf43e944dcd7d62eb666d80a7807e462aabcde0
stegmuel <thomas.steg87@gmail.com>
2020-05-11 23:05:50 +0200

    Modified the gan trainer

--- generate_single_track.py
+++ generate_single_track.py
@@ -1,12 +1,6 @@
-import os
-import torch
-import shutil
-from create_maestro_file import create_modified_midifile, convert_midi_to_wav, cut_track_and_stack
-from processing.post_processing import overlap_and_add_samples
-from scipy.io.wavfile import write
-from utils.utils import get_generator
 import argparse
 from processing.post_processing import generate_single_track
+
 
 def main():
     midi_filepath = '/media/thomas/Samsung_T5/VITA/data/maestro-v1.0.0/2017/MIDI-Unprocessed_083_PIANO083_MID--AUDIO' \
@@ -20,13 +14,13 @@ def main():
 
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser(description='Train a deep clustering autoencoder for a given number of epochs')
-    # parser.add_argument('epochs', metavar='epochs', type=int, help='Number of epochs to train the encoder_model on')
-    # parser.add_argument('data_path', metavar='data_path', type=str, help='Path to data directory')
-    # parser.add_argument('filepath', metavar='filepath', type=str, help='Path to hdf5 file')
-    # parser.add_argument('models_path', metavar='models_path', type=str, help='Path to models')
-    # parser.add_argument('dec_name', metavar='dec_name', type=str, help='Name of the DEC encoder_model')
-    # args = parser.parse_args()
-    #
-    # main(args)
+    parser = argparse.ArgumentParser(description='Train a deep clustering autoencoder for a given number of epochs')
+    parser.add_argument('epochs', metavar='epochs', type=int, help='Number of epochs to train the encoder_model on')
+    parser.add_argument('data_path', metavar='data_path', type=str, help='Path to data directory')
+    parser.add_argument('filepath', metavar='filepath', type=str, help='Path to hdf5 file')
+    parser.add_argument('models_path', metavar='models_path', type=str, help='Path to models')
+    parser.add_argument('dec_name', metavar='dec_name', type=str, help='Name of the DEC encoder_model')
+    args = parser.parse_args()
+
+    main(args)
     main()
