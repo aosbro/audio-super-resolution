9b6ef31b3c5679f043c60d873eae953096a5dbd6
stegmuel <thomas.steg87@gmail.com>
2020-05-11 21:20:44 +0200

    Modified the gan trainer

--- generate_single_track.py
+++ generate_single_track.py
@@ -5,18 +5,25 @@ from create_maestro_file import create_modified_midifile, convert_midi_to_wav, c
 from processing.post_processing import overlap_and_add_samples
 from scipy.io.wavfile import write
 from utils.utils import get_generator
+import argparse
 
 
-def main():
-    midi_filepath = '/media/thomas/Samsung_T5/VITA/data/maestro-v1.0.0/2017/MIDI-Unprocessed_083_PIANO083_MID--AUDIO' \
-                    '-split_07-09-17_Piano-e_2_-06_wav--3.midi'
-    temporary_directory_path = 'data/temp'
-
-    transformations = {'input': {'instrument': 4, 'velocity': None, 'control': None, 'control_value': None},
-                       'target': {'instrument': 0, 'velocity': None, 'control': None, 'control_value': None}}
-
+def generate_single_track(original_midi_filepath, temporary_directory_path, transformations, generator_path, device):
+    """
+    Generates a part of single track using a pre-trained generator starting from the original .midi file. The
+    transformation to apply to get the (input, target) pair of signals are specified in the transformations dictionary.
+    Ideally the transformation should be the same as the one used to train the generator. The temporary directory is
+    used to store the temporary .wav and .midi files. Caution: Its content will be removed before creating the new
+    files. The generated track will be saved inside the temporary directory.
+    :param original_midi_filepath: location of the original .midi file (string).
+    :param temporary_directory_path: location of the temporary directory (string).
+    :param transformations: dictionary of transformations to apply to the 'input' and 'target' tracks (dictionary).
+    :param generator_path: location of the generator trainer to restore pre-trained weights (string).
+    :param device: either 'cpu' or 'cuda' depending on hardware availability
+    :return:None
+    """
     # Load the pre-trained generator
-    generator = get_generator(loadpath='objects/generator_trainer_time_freq.tar', device='cpu')
+    generator = get_generator(loadpath=generator_path, device=device)
 
     # Create a temporary directory
     if os.path.exists(temporary_directory_path):
@@ -24,10 +31,10 @@ def main():
     os.mkdir(temporary_directory_path)
 
     # Generate the pair of .midi files
-    create_modified_midifile(midi_filepath=midi_filepath,
+    create_modified_midifile(midi_filepath=original_midi_filepath,
                              midi_savepath=os.path.join(temporary_directory_path, 'input.midi'),
                              **transformations['input'])
-    create_modified_midifile(midi_filepath=midi_filepath,
+    create_modified_midifile(midi_filepath=original_midi_filepath,
                              midi_savepath=os.path.join(temporary_directory_path, 'target.midi'),
                              **transformations['target'])
 
@@ -53,5 +60,25 @@ def main():
     write(os.path.join(temporary_directory_path, 'generated.wav'), fs, full_sample_generated.numpy())
 
 
+def main():
+    midi_filepath = '/media/thomas/Samsung_T5/VITA/data/maestro-v1.0.0/2017/MIDI-Unprocessed_083_PIANO083_MID--AUDIO' \
+                    '-split_07-09-17_Piano-e_2_-06_wav--3.midi'
+    temporary_directory_path = 'data/temp'
+    generator_path = 'objects/generator_trainer_time2.tar'
+    transformations = {'input': {'instrument': 4, 'velocity': None, 'control': None, 'control_value': None},
+                       'target': {'instrument': 0, 'velocity': None, 'control': None, 'control_value': None}}
+
+    generate_single_track(midi_filepath, temporary_directory_path, transformations, generator_path, 'cpu')
+
+
 if __name__ == '__main__':
+    # parser = argparse.ArgumentParser(description='Train a deep clustering autoencoder for a given number of epochs')
+    # parser.add_argument('epochs', metavar='epochs', type=int, help='Number of epochs to train the encoder_model on')
+    # parser.add_argument('data_path', metavar='data_path', type=str, help='Path to data directory')
+    # parser.add_argument('filepath', metavar='filepath', type=str, help='Path to hdf5 file')
+    # parser.add_argument('models_path', metavar='models_path', type=str, help='Path to models')
+    # parser.add_argument('dec_name', metavar='dec_name', type=str, help='Name of the DEC encoder_model')
+    # args = parser.parse_args()
+    #
+    # main(args)
     main()
--- train_gan.py
+++ train_gan.py
@@ -1,5 +1,5 @@
 import os
-from utils.utils import get_the_data_loaders
+from utils.utils import get_the_maestro_data_loaders_hdf, get_the_maestro_data_loaders_npy
 from utils.constants import *
 from models.generator import Generator
 from models.autoencoder import AutoEncoder
@@ -7,7 +7,6 @@ from models.discriminator import Discriminator
 from trainers.base_trainer import Trainer
 from torch.optim import lr_scheduler
 from torch import nn
-from torch.nn.utils import clip_grad_norm_
 import torch
 from torchaudio.transforms import Spectrogram
 import numpy as np
@@ -62,7 +61,7 @@ class GanTrainer(Trainer):
 
         # Define labels
         self.real_label = 1
-        self.fake_label = 0
+        self.generated_label = 0
 
         # Loss scaling factors
         self.lambda_adv = LAMBDA_ADVERSARIAL_MIN
@@ -88,12 +87,11 @@ class GanTrainer(Trainer):
         for epoch in range(epochs):
             self.generator.train()
             self.discriminator.train()
-            train_loader_iter = iter(self.train_loader)
             for i in range(TRAIN_BATCH_ITERATIONS):
                 # Transfer to GPU
-                local_batch = next(train_loader_iter)
-                x_h_batch, x_l_batch = local_batch[0].to(self.device), local_batch[1].to(self.device)
-                batch_size = x_h_batch.shape[0]
+                local_batch = next(self.train_loader_iter)
+                input_batch, target_batch = local_batch[0].to(self.device), local_batch[1].to(self.device)
+                batch_size = input_batch.shape[0]
 
                 ############################
                 # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
@@ -101,7 +99,7 @@ class GanTrainer(Trainer):
                 # Train the discriminator with real data
                 self.discriminator_optimizer.zero_grad()
                 label = torch.full((batch_size,), self.real_label, device=self.device)
-                output = self.discriminator(x_h_batch)
+                output = self.discriminator(input_batch)
 
                 # Compute and store the discriminator loss on real data
                 loss_discriminator_real = self.adversarial_criterion(torch.squeeze(output), label)
@@ -109,14 +107,14 @@ class GanTrainer(Trainer):
                 loss_discriminator_real.backward()
 
                 # Train the discriminator with fake data
-                fake_batch = self.generator(x_l_batch)
-                label.fill_(self.fake_label)
-                output = self.discriminator(fake_batch.detach())
+                generated_batch = self.generator(target_batch)
+                label.fill_(self.generated_label)
+                output = self.discriminator(generated_batch.detach())
 
                 # Compute and store the discriminator loss on fake data
-                loss_discriminator_fake = self.adversarial_criterion(torch.squeeze(output), label)
-                self.train_losses['discriminator_adversarial']['fake'].append(loss_discriminator_fake.item())
-                loss_discriminator_fake.backward()
+                loss_discriminator_generated = self.adversarial_criterion(torch.squeeze(output), label)
+                self.train_losses['discriminator_adversarial']['fake'].append(loss_discriminator_generated.item())
+                loss_discriminator_generated.backward()
 
                 # Update the discriminator weights
                 self.discriminator_optimizer.step()
@@ -127,12 +125,12 @@ class GanTrainer(Trainer):
                 self.generator_optimizer.zero_grad()
 
                 # Get the spectrogram
-                specgram_h_batch = self.spectrogram(x_h_batch)
-                specgram_fake_batch = self.spectrogram(fake_batch)
+                specgram_target_batch = self.spectrogram(target_batch)
+                specgram_fake_batch = self.spectrogram(generated_batch)
 
                 # Fake labels are real for the generator cost
                 label.fill_(self.real_label)
-                output = self.discriminator(fake_batch)
+                output = self.discriminator(generated_batch)
 
                 # Compute the generator loss on fake data
                 # Get the adversarial loss
@@ -142,21 +140,21 @@ class GanTrainer(Trainer):
                 self.train_losses['generator_adversarial'].append(loss_generator_adversarial.item())
 
                 # Get the L2 loss in time domain
-                loss_generator_time = self.generator_time_criterion(fake_batch, x_h_batch)
+                loss_generator_time = self.generator_time_criterion(generated_batch, target_batch)
                 self.train_losses['time_l2'].append(loss_generator_time.item())
 
                 # Get the L2 loss in frequency domain
-                loss_generator_frequency = self.generator_frequency_criterion(specgram_fake_batch, specgram_h_batch)
+                loss_generator_frequency = self.generator_frequency_criterion(specgram_fake_batch, specgram_target_batch)
                 self.train_losses['freq_l2'].append(loss_generator_frequency.item())
 
                 # Get the L2 loss in embedding space
                 loss_generator_autoencoder = torch.zeros(size=[1], device=self.device)
                 if self.use_autoencoder:
                     # Get the embeddings
-                    _, embedding_h_batch = self.autoencoder(x_h_batch)
-                    _, embedding_fake_batch = self.autoencoder(fake_batch)
-                    loss_generator_autoencoder = self.generator_autoencoder_criterion(embedding_fake_batch,
-                                                                                      embedding_h_batch)
+                    _, embedding_target_batch = self.autoencoder(target_batch)
+                    _, embedding_generated_batch = self.autoencoder(generated_batch)
+                    loss_generator_autoencoder = self.generator_autoencoder_criterion(embedding_generated_batch,
+                                                                                      embedding_target_batch)
                     self.train_losses['autoencoder_l2'].append(loss_generator_autoencoder.item())
 
                 # Combine the different losses
@@ -184,14 +182,8 @@ class GanTrainer(Trainer):
                                                        loss_generator_autoencoder.item(),
                                                        loss_generator_adversarial.item(),
                                                        loss_discriminator_real.item(),
-                                                       loss_discriminator_fake.item())
+                                                       loss_discriminator_generated.item())
                     print(message)
-
-            # Activate the coupling between discriminator and generator
-            # if self.epoch >= ADVERSARIAL_ACTIVATION_EPOCH:
-            #     self.use_adversarial = True
-            #     if self.lambda_adv < LAMBDA_ADVERSARIAL_MAX:
-            #         self.lambda_adv *= 10
 
             # Evaluate the model
             with torch.no_grad():
@@ -278,11 +270,14 @@ class GanTrainer(Trainer):
         self.valid_losses = checkpoint['valid_losses']
 
 
-def get_gan_trainer(train_datapath, test_datapath, valid_datapath, loadpath, savepath, batch_size, generator_path,
-                    autoencoder_path):
-    # Create the datasets
-    train_loader, test_loader, valid_loader = get_the_data_loaders(train_datapath, test_datapath, valid_datapath,
-                                                                   batch_size)
+def get_gan_trainer(datapath, loadpath, savepath, datasets_parameters, loaders_parameters, generator_path=None,
+                    autoencoder_path=None, use_hdf5=None):
+    # Get the data loader for each phase
+    if use_hdf5:
+        train_loader, test_loader, valid_loader = get_the_maestro_data_loaders_hdf(datapath, datasets_parameters,
+                                                                                   loaders_parameters)
+    else:
+        train_loader, test_loader, valid_loader = get_the_maestro_data_loaders_npy(datapath, loaders_parameters)
 
     gan_trainer = GanTrainer(train_loader=train_loader,
                              test_loader=test_loader,
@@ -292,33 +287,23 @@ def get_gan_trainer(train_datapath, test_datapath, valid_datapath, loadpath, sav
                              savepath=savepath,
                              generator_path=generator_path,
                              autoencoder_path=autoencoder_path)
-    return gan_trainer
 
-
-def train_gan(train_datapath, test_datapath, valid_datapath, loadpath, savepath, epochs, batch_size, generator_path,
-              autoencoder_path):
-    gan_trainer = get_gan_trainer(train_datapath=train_datapath,
-                                  test_datapath=test_datapath,
-                                  valid_datapath=valid_datapath,
-                                  loadpath=loadpath,
-                                  savepath=savepath,
-                                  batch_size=batch_size,
-                                  generator_path=generator_path,
-                                  autoencoder_path=autoencoder_path)
-
-    # Start training
-    gan_trainer.train(epochs=epochs)
     return gan_trainer
 
 
 if __name__ == '__main__':
-    gan_trainer = train_gan(train_datapath=TRAIN_DATAPATH,
-                            test_datapath=TEST_DATAPATH,
-                            valid_datapath=VALID_DATAPATH,
-                            loadpath=GAN_PATH,
-                            savepath=GAN_PATH,
-                            epochs=1,
-                            batch_size=16,
-                            generator_path=GENERATOR_L2T_PATH,
-                            autoencoder_path=AUTOENCODER_L2TF_PATH)
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+    datapath = {phase: os.path.join('data', phase + '.npy') for phase in ['train', 'test', 'valid']}
+    datasets_parameters = {phase: {'batch_size': 64, 'use_cache': True} for phase in ['train', 'test', 'valid']}
+    loaders_parameters = {phase: {'batch_size': 64, 'shuffle': False, 'num_workers': 2}
+                          for phase in ['train', 'test', 'valid']}
+
+    generator_trainer = get_gan_trainer(datapath=datapath,
+                                        loadpath='',
+                                        savepath='',
+                                        datasets_parameters=datasets_parameters,
+                                        loaders_parameters=loaders_parameters,
+                                        use_hdf5=False)
+    # generator_trainer.train(epochs=1)
 
